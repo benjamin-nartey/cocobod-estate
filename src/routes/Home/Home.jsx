214e31e74f67b6e337b20c1e2c8bcc159411108c
benjamin-nartey <nartbillionz@gmail.com>
2023-06-07 13:51:38 +0000

    modify:changed the layout of the sidebar

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -4,6 +4,9 @@ import { NavLink } from "react-router-dom/dist";
 import { categories } from "../../utils/categories";
 import { Outlet } from "react-router-dom/dist";
 
+// const isNotActiveStyle = "hover:text-[#6e431d]";
+// const isActiveStyle = "text-[#6e431d] font-semibold";
+
 function Home() {
   return (
     <div className="w-full p-5">
@@ -11,7 +14,7 @@ function Home() {
       <div className="flex gap-8 my-5 capitalize text-gray-400 justify-start items-center">
         {categories.map((category) => (
           <NavLink
-            className="hover:text-[#6E431D] "
+            className="hover:text-[#6e431d] focus:text-[#6e431d] focus:font-semibold transition-all"
             to={`/home/category/${category.name}`}
           >
             {category.name}
