15984a191f7f6d45a4ce3e436b7b5c6dacbab5ab
benjamin-nartey <nartbillionz@gmail.com>
2023-06-27 08:27:53 +0000

    added some padding to Gallery div

--- src/routes/Gallery/Gallery.jsx
+++ src/routes/Gallery/Gallery.jsx
@@ -6,11 +6,11 @@ function Gallery() {
   const [propertyData, setPropertyData] = useLocalStorage("propertyData", null);
 
   return (
-    <div className="w-full relative p-4">
+    <div className="w-full relative p-2">
       <MasonryLayout>
         {propertyData.gallery.map((property, id) => (
-          <div key={id} className="relative cursor-pointer w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out m-2">
-            {<img src={property.url} className="w-max h-auto" />}
+          <div key={id} className="relative cursor-pointer w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out p-2">
+            {<img src={property.url} className="w-max h-auto rounded-lg" />}
           </div>
         ))}
       </MasonryLayout>
