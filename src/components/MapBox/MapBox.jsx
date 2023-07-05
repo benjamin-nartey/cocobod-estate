8156279b35396ef071c5e70fb302936ecc39cdcc
benjamin-nartey <nartbillionz@gmail.com>
2023-07-05 14:45:30 +0000

    added Map Marker

--- src/components/MapBox/MapBox.jsx
+++ src/components/MapBox/MapBox.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Map, { Marker } from 'react-map-gl';
-import { useState } from 'react';
-import { useContext } from 'react';
-import { SearchResultContext } from '../../context/searchResult.context';
-import axiosFetch from '../../axios/axios';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { getCenter } from 'geolib';
-import { useEffect } from 'react';
+import React from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import { useState } from "react";
+import { useContext } from "react";
+import { SearchResultContext } from "../../context/searchResult.context";
+import axiosFetch from "../../axios/axios";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { getCenter } from "geolib";
+import { useEffect } from "react";
 
 const defaultViewState = {
   latitude: 0,
@@ -31,7 +31,7 @@ function MapBox({ searchResult }) {
     setCenter(getCenter(coordinates));
   }, [searchResult]);
 
-  console.log('center', center);
+  console.log("center", center);
 
   const [viewState, setViewState] = useState({
     latitude: center && center?.latitude,
@@ -52,14 +52,28 @@ function MapBox({ searchResult }) {
   return (
     <Map
       {...viewState}
-      mapLib={import('mapbox-gl')}
+      mapLib={import("mapbox-gl")}
       // initialViewState={viewState}
       // viewState={viewState}
       onMove={(evt) => setViewState(evt.viewState)}
       className="w-full h-full"
       mapStyle="mapbox://styles/brightarhin/clje8v5tp005d01qsaa1ya2u8"
       mapboxAccessToken="pk.eyJ1IjoiYnJpZ2h0YXJoaW4iLCJhIjoiY2w4OHM3Z3dlMDA0YTNubjFjcWZoYjNnOSJ9.mYzqF1JVp18gZ9-ZUqMXZw"
-    />
+    >
+      {searchResult &&
+        searchResult.map((result) => (
+          <div>
+            <Marker
+              longitude={parseFloat(result.location.long)}
+              latitude={parseFloat(result.location.lat)}
+              offsetLeft={-20}
+              offsetRight={-10}
+            >
+              <p className="animate-bounce delay-300"> 📌</p>
+            </Marker>
+          </div>
+        ))}
+    </Map>
   );
 }
 
