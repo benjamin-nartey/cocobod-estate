5a695aa9f1ab3d642bd6c238c6f0a9e6093deb87
Bright Arhin <barhin49@gmail.com>
2023-07-05 12:56:49 +0000

    fix map centering

--- src/components/MapBox/MapBox.jsx
+++ src/components/MapBox/MapBox.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Map, { Marker } from "react-map-gl";
-import { useState } from "react";
-import { useContext } from "react";
-import { SearchResultContext } from "../../context/searchResult.context";
-import axiosFetch from "../../axios/axios";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { getCenter } from "geolib";
-import { useEffect } from "react";
+import React from 'react';
+import Map, { Marker } from 'react-map-gl';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { SearchResultContext } from '../../context/searchResult.context';
+import axiosFetch from '../../axios/axios';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { getCenter } from 'geolib';
+import { useEffect } from 'react';
 
 const defaultViewState = {
   latitude: 0,
@@ -19,18 +19,19 @@ function MapBox({ searchResult }) {
   // const [centerLatitude, setCenterLatitude] = useState(null);
   // const [centerLongitude, setCenterLongitude] = useState(null);
   const [center, setCenter] = useState(null);
-  console.log("init", searchResult);
 
-  const coordinates = searchResult?.map((result) => ({
-    latitude: Number(result?.location?.lat),
-    longitude: Number(result?.location?.long),
-  }));
+  // console.log('init', searchResult);
 
   useEffect(() => {
-    setCenter(getCenter(coordinates));
-  }, [center]);
+    const coordinates = searchResult?.map((result) => ({
+      latitude: Number(result?.location?.lat),
+      longitude: Number(result?.location?.long),
+    }));
 
-  console.log("center", center);
+    setCenter(getCenter(coordinates));
+  }, [searchResult]);
+
+  console.log('center', center);
 
   const [viewState, setViewState] = useState({
     latitude: center && center?.latitude,
@@ -38,15 +39,23 @@ function MapBox({ searchResult }) {
     zoom: 6,
   });
 
+  useEffect(() => {
+    setViewState({
+      latitude: center && center?.latitude,
+      longitude: center && center?.longitude,
+      zoom: 6,
+    });
+  }, [center]);
+
   console.log({ viewState });
 
   return (
     <Map
       {...viewState}
-      mapLib={import("mapbox-gl")}
+      mapLib={import('mapbox-gl')}
       // initialViewState={viewState}
       // viewState={viewState}
-      // onMove={(evt) => setViewState(evt.viewState)}
+      onMove={(evt) => setViewState(evt.viewState)}
       className="w-full h-full"
       mapStyle="mapbox://styles/brightarhin/clje8v5tp005d01qsaa1ya2u8"
       mapboxAccessToken="pk.eyJ1IjoiYnJpZ2h0YXJoaW4iLCJhIjoiY2w4OHM3Z3dlMDA0YTNubjFjcWZoYjNnOSJ9.mYzqF1JVp18gZ9-ZUqMXZw"
