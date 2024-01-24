2275cec5f9d01e862e2f4d3494df1e11331f4184
Bright Arhin <barhin49@gmail.com>
2024-01-24 11:49:16 +0000

    baseURL fix

--- src/Hooks/useLogout.js
+++ src/Hooks/useLogout.js
@@ -8,7 +8,7 @@ export const useLogout = async () => {
   const refreshToken = JSON.parse(localStorage.getItem('refreshToken'));
   const api = axios.create({
     // baseURL: 'https://estate-api-2.onrender.com/api/v1/',
-    baseURL: 'http://localhost:3000/api/v1/',
+    baseURL: 'http://nest-app:3000/api/v1/',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${refreshToken}`,
