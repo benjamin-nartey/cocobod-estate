1d01dd1d5992c3037ef318ae1a7d5d4d4be89f3c
Bright Arhin <barhin49@gmail.com>
2024-02-12 10:47:36 +0000

    changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8me71r6qav"
+    "revision": "0.vtjdviplqeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
